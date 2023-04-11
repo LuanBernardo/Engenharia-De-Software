c2d95c96fa0783653dc01963cb1e8603224c1c3b
LuanBernardo <73596241+LuanBernardo@users.noreply.github.com>
2023-04-11 00:34:54 -0300

    Add files via upload

---
+++ identifyTheBiggerNumber.py
@@ -0,0 +1,9 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if (num1 > num2):
+    print("O primeiro número é maior: ", num1)
+elif (num1 < num2):
+    print("O segundo número é maior: ", num2)
+else:
+    print("Os numeros são iguais")
